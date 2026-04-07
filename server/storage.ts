3b2d19459b0ea4d83556b9658a5f993037d21cab
dorotahmatula <dorota.h.matula@gmail.com>
2026-04-07 19:40:13 +0200

    Add files via upload

--- server/storage.ts
+++ server/storage.ts
@@ -1,19 +1,5 @@
-import { newsletterSubscribers } from "@shared/schema";
-import { db } from "./db";
+export interface IStorage {}
 
-export interface IStorage {
-  subscribeNewsletter(email: string): Promise<any>;
-}
-
-export class DatabaseStorage implements IStorage {
-  async subscribeNewsletter(email: string) {
-    const [subscriber] = await db
-      .insert(newsletterSubscribers)
-      .values({ email })
-      .onConflictDoNothing()
-      .returning();
-    return subscriber || { email, alreadySubscribed: true };
-  }
-}
+export class DatabaseStorage implements IStorage {}
 
 export const storage = new DatabaseStorage();
